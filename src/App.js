6acbc83d41e0a8c5a617f7bd2b3268f89cd833ce
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-03 14:10:56 +0530

    Add className to the card-list

--- src/App.js
+++ src/App.js
@@ -22,8 +22,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <CardList className="card-list">
-          { this.state.monsters.map(monster => <h1 key={monster.id} > {monster.name} </h1>)}
+        <CardList monsters={this.state.monsters}>
         </CardList>
       </div>
     );
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './card-list.styles.css'
+import { Card } from "../card/card";
 
 export const CardList = (props) => {
-  return <div>{props.children}</div>
+  return  <div className="card-list">{props.monsters.map(monster => {
+              return <h1 key={monster.id} > {monster.name} </h1>})}
+          </div>
 }
