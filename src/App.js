662c0ae4322bbe565117fb4e9865a445fd757190
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-03 14:04:41 +0530

    Add card component

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <CardList>
+        <CardList className="card-list">
           { this.state.monsters.map(monster => <h1 key={monster.id} > {monster.name} </h1>)}
         </CardList>
       </div>
